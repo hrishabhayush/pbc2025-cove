6bda956fda056b17b17d3a5e8867466cfb391da7
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-02-18 13:04:40 -0500

    constructor and setPremium function added

--- src/Fly.sol
+++ src/Fly.sol
@@ -22,6 +22,7 @@ contract Fly is ReentrancyGuard {
         suint256 flightId;
         suint256 insurancePremium;
         sbool insuranceStatus;
+        suint256 flightPrice;
     } 
 
     saddress[] passengers;
@@ -32,11 +33,43 @@ contract Fly is ReentrancyGuard {
     // all the policies corresponding to that flight has been resolved
     mapping(suint256 => sbool) flightStatus;
 
-    modifier onlyPassengers() {
-        uint i = 0;
-        while (suint256(i) < passengers.length) {
-            require(saddress(msg.sender)==passengers[suint256(i)], "You're not one of the passengers");
+    modifier onlyPassenger() {
+        sbool isPassenger;
+        for (uint i = 0; suint(i) < passengers.length; i++) {
+            if (saddress(msg.sender)==passengers[i]) {
+                isPassenger = sbool(true);
+                break;
+            }
+            require(isPassenger, "You're not one of the passengers");
             _;
         }
     }
-}
+
+    modifier onlyAdmin() {
+        require(saddress(msg.sender)==adminAddress, "You are not the admin");
+        _;
+    }
+
+    modifier onlyProviders() {
+        sbool isProvider;
+        for (uint i = 0; suint(i) < providers.length; i++) {
+            if (saddress(msg.sender)==providers[i]) {
+                isProvider = sbool(true);
+                break;
+            }
+            require(isProvider, "You're not one of the providers");
+            _;
+        }
+    }
+
+    constructor(saddress _adminAddress, saddress[] memory _passengers, saddress[] memory _providers) {
+        adminAddress = _adminAddress;
+        passengers = _passengers;
+        _providers = _providers;
+    }
+
+    function setPremium(suint256 id, suint256 fee, sbool status) external onlyPassenger {
+        policies[saddress(msg.sender)].flightId = id;
+        policies[saddress(msg.sender)].insurancePremium = fee;
+    }
+ }
