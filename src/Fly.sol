7dc60d09364dffcf00f571dd628abd40fbbeaeb4
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-02-18 12:47:05 -0500

    stypes updated and modifier added

--- src/Fly.sol
+++ src/Fly.sol
@@ -16,26 +16,27 @@ contract Fly is ReentrancyGuard {
     // Fixed point arithmetic unit
     suint256 wad;
 
+    saddress provider;
+
     struct Policy {
-        saddress passenger;
         suint256 flightId;
         suint256 insurancePremium;
-        suint256 payout;
         sbool insuranceStatus;
     } 
 
-    mapping(suint256 => Policy) policies;
-    mapping(saddress => suint256) insurerBalances;
+    saddress[] passengers;
+    saddress[] providers;
+    mapping(saddress => Policy) policies; // Each passenger what policy they hold
+
+    // Corresponding to each flight we maintain a boolean, that tells us whether 
+    // all the policies corresponding to that flight has been resolved
     mapping(suint256 => sbool) flightStatus;
 
-    
-    constructor(Policy policy) {
-
-    }
-
-
-
-    constructor() {
-
+    modifier onlyPassengers() {
+        uint i = 0;
+        while (suint256(i) < passengers.length) {
+            require(saddress(msg.sender)==passengers[suint256(i)], "You're not one of the passengers");
+            _;
+        }
     }
 }
