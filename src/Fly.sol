441ff694024328f7d0f42c3f53e9c13f7656a00b
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-02-18 12:15:02 -0500

    updated lib

--- src/Fly.sol
+++ src/Fly.sol
@@ -6,5 +6,36 @@ import "lib/solmate/src/utils/ReentrancyGuard.sol";
 import "./SRC20.sol";
 
 contract Fly is ReentrancyGuard {
+    
+    // Add some stypes here
 
+    SRC20 public flyAsset;
+
+    saddress adminAddress;
+
+    // Fixed point arithmetic unit
+    suint256 wad;
+
+    struct Policy {
+        saddress passenger;
+        suint256 flightId;
+        suint256 insurancePremium;
+        suint256 payout;
+        sbool insuranceStatus;
+    } 
+
+    mapping(suint256 => Policy) policies;
+    mapping(saddress => suint256) insurerBalances;
+    mapping(suint256 => sbool) flightStatus;
+
+    
+    constructor(Policy policy) {
+
+    }
+
+
+
+    constructor() {
+
+    }
 }
