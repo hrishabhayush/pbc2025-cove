35ca2f4a13a2e1b702678ae9b280d18a2a76ce4e
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-02-19 10:43:51 -0500

    Logic added for Cove Coin

--- packages/contracts/script/Fly.s.sol
+++ packages/contracts/script/Fly.s.sol
@@ -1,28 +1,40 @@
 // // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "lib/forge-std/src/Script.sol";
+import {Script, console} from "forge-std/src/Script.sol";
 import {Fly} from "../src/Fly.sol";
+import {SRC20} from "../src/SRC20.sol";
 
 contract FlyScript is Script {
     Fly public fly;
+    SRC20 public flyAsset;
 
     function setUp() public {
-        saddress admin = saddress(0x14dC79964da2C08b23698B3D3cc7Ca32193d9955);
+        Fly public fly;
+        address admin = address(0x14dC79964da2C08b23698B3D3cc7Ca32193d9955);
 
-        saddress[] memory passengers = new address[](1);
-        passengers[suint256(0)] = saddress(0x456);
+        address[] memory passengers = new address[](1);
+        passengers[0] = address(0x456);
 
-        saddress[] memory providers = new address[](1);
-        providers[suint256(0)] = saddress(0x789);
+        address[] memory providers = new address[](1);
+        providers[0] = address(0x789);
 
-        suint256 constant WAD = 1e18;
+        // Deploy the SRC20 token contract
+        flyAsset = new SRC20("Cove Token", "COVE", 18);
 
-        fly = new Fly(adminAddress, passengers, providers, );
+        // Deploy the Fly contract
+        fly = new Fly(admin, passengers, providers, flyAsset);
     }
 
     function run() public {
         vm.startBroadcast();
+
+        fly.createPolicy(suint256(1), suint256(1), suint256(1e16), suint256(1e20));
+        fly.createPolicy(suint256(2), suint256(2), suint256(1e17), suint256(1e18));
+        fly.createPolicy(suint256(3), suint256(1), suint256(1e19), suint256(1e20));
+        fly.createPolicy(suint256(4), suint256(1), suint256(1e18), suint256(1e20));
+        fly.createPolicy(suint256(5), suint256(1), suint256(1e19), suint256(1e20));
+        fly.createPolicy(suint256(6), suint256(1), suint256(1e17), suint256(1e20));
 
         vm.stopBroadcast();
     }
---
+++ packages/contracts/src/CoveCoin.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.13;
+
+import {SRC20, ISRC20} from "./SRC20.sol";
+
+/*//////////////////////////////////////////////////////////////
+//                      ICoveCoin Interface
+//////////////////////////////////////////////////////////////*/
+
+// IViolinCoin extends ISRC20 by adding the mint function.
+interface ICoveCoin is ISRC20 {
+    function mint(saddress to, suint256 amount) external;
+}
+/*//////////////////////////////////////////////////////////////
+//                        CoveCoin Contract
+//////////////////////////////////////////////////////////////*/
+
+contract CoveCoin is SRC20, ICoveCoin {
+    address public owner;
+
+    constructor(address _owner, string memory _name, string memory _symbol, uint8 _decimals)
+        SRC20(_name, _symbol)
+    {
+        owner = _owner;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Must be owner");
+        _;
+    }
+
+    /// @notice Mints new tokens to the specified address.
+    function mint(saddress to, suint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    /// @notice Returns the balance of msg.sender.
+    function balanceOf() internal view returns (suint256) {
+        return super.balanceOf();
+    }
+
+    /// @notice Transfers tokens to another address.
+    function transfer(saddress to, suint256 amount) public override(ISRC20, SRC20) returns (bool) {
+        return super.transfer(to, amount);
+    }
+
+    /// @notice Transfers tokens from one address to another.
+    function transferFrom(saddress from, saddress to, suint256 amount) public override(ISRC20, SRC20) returns (bool) {
+        return super.transferFrom(from, to, amount);
+    }
+}
