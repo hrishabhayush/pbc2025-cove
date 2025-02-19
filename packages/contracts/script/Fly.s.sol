856f8e2edf05f990f2566c9cebe3fb1448ea38c9
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-02-19 09:10:15 -0500

    failing FlyScript

--- packages/contracts/script/Fly.s.sol
+++ packages/contracts/script/Fly.s.sol
@@ -1,29 +1,29 @@
 // // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+pragma solidity ^0.8.13;
 
-// import {Script, console} from "lib/forge-std/src/Script.sol";
-// import {Fly} from "../src/Fly.sol";
+import {Script, console} from "lib/forge-std/src/Script.sol";
+import {Fly} from "../src/Fly.sol";
 
-// contract FlyScript is Script {
-//     Fly public fly;
+contract FlyScript is Script {
+    Fly public fly;
 
-//     function setUp() public {
-//         saddress admin = saddress(0x14dC79964da2C08b23698B3D3cc7Ca32193d9955);
+    function setUp() public {
+        saddress admin = saddress(0x14dC79964da2C08b23698B3D3cc7Ca32193d9955);
 
-//         saddress[] memory passengers = new address[](1);
-//         passengers[suint256(0)] = saddress(0x456);
+        saddress[] memory passengers = new address[](1);
+        passengers[suint256(0)] = saddress(0x456);
 
-//         saddress[] memory providers = new address[](1);
-//         providers[suint256(0)] = saddress(0x789);
+        saddress[] memory providers = new address[](1);
+        providers[suint256(0)] = saddress(0x789);
 
-//         suint256 constant WAD = 1e18;
+        suint256 constant WAD = 1e18;
 
-//         fly = new Fly(adminAddress, passengers, providers, );
-//     }
+        fly = new Fly(adminAddress, passengers, providers, );
+    }
 
-//     function run() public {
-//         vm.startBroadcast();
+    function run() public {
+        vm.startBroadcast();
 
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.stopBroadcast();
+    }
+}
