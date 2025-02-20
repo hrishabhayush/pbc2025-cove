c515168dd222ecf48d4d3d93266fbc96ff72c626
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-02-19 22:47:40 -0500

    sforge clean, and sforge build run

--- packages/contracts/script/Cove.s.sol
+++ packages/contracts/script/Cove.s.sol
@@ -9,27 +9,18 @@ contract CoveScript is Script {
     Cove public cove;
     CoveCoin public coveAsset;
 
-    function setUp() public {
-        address admin = address(0x14dC79964da2C08b23698B3D3cc7Ca32193d9955);
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVKEY");
 
+        vm.startBroadcast(deployerPrivateKey);
+
+        saddress admin = saddress(0x123);
         saddress[] memory passengers = new saddress[](suint256(1));
         passengers[suint256(0)] = saddress(0x456);
-
         saddress[] memory providers = new saddress[](suint256(1));
         providers[suint256(0)] = saddress(0x789);
 
-        cove = new Cove(saddress(admin), passengers, providers, coveAsset);
-    }
-
-    function run() public {
-        vm.startBroadcast();
-
-        cove.createPolicy(suint256(1), suint256(1), suint256(1e16), suint256(1e20));
-        cove.createPolicy(suint256(2), suint256(2), suint256(1e17), suint256(1e18));
-        cove.createPolicy(suint256(3), suint256(1), suint256(1e19), suint256(1e20));
-        cove.createPolicy(suint256(4), suint256(1), suint256(1e18), suint256(1e20));
-        cove.createPolicy(suint256(5), suint256(1), suint256(1e19), suint256(1e20));
-        cove.createPolicy(suint256(6), suint256(1), suint256(1e17), suint256(1e20));
+        cove = new Cove(admin, passengers, providers, coveAsset);
 
         vm.stopBroadcast();
     }
